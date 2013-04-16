25017b9e6c948fdf51512ccbede86bcba8114e7b
Mathieu Gosbee <mail@matbee.com>
2013-04-15 21:09:15 -0400

    .

--- client.js
+++ client.js
@@ -6,27 +6,6 @@ var wormhole = function (socket) {
 	this.rpc = {};
 	this.callback = function () {};
 	var self = this;
-	var globalEvent = "*";
-	socket.$emit = function (name) {
-	    if(!this.$events) return false;
-	    for(var i=0;i<2;++i){
-	        if(i==0 && name==globalEvent) continue;
-	        var args = Array.prototype.slice.call(arguments, 1-i);
-	        var handler = this.$events[i==0?name:globalEvent];
-	        if(!handler) handler = [];
-	        if ('function' == typeof handler) handler.apply(this, args);
-	        else if (handler && handler.length >= 0 && handler.slice) {
-	            var listeners = handler.slice();
-	            for (var i=0, l=listeners.length; i<l; i++)
-	                listeners[i].apply(this, args);
-	        } else return false;
-	    }
-	    return true;
-	};
-	socket.on(globalEvent,function(event){
-	    var args = Array.prototype.slice.call(arguments, 1);
-	    console.log("Global Event = "+event+"; Arguments = "+JSON.stringify(args));
-	});
 	this.setupSocket(socket);
 };
 wormhole.prototype.getSocket = function () {
