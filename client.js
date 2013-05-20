88f4127b560fc665339189919856028a8261bc10
Mathieu Gosbee <mail@matbee.com>
2013-05-20 16:20:12 -0400

    omg retainer ice is gross

--- client.js
+++ client.js
@@ -93,7 +93,7 @@ wormhole.prototype.executeRpc = function(methodName, isAsync, args, uuid) {
 wormhole.prototype.syncClientRpc = function (data) {
 	var self = this;
 	for (var k in data) {
-		this.clientFunctions[k] = eval("(function () { return " + data[k] + " }())");
+		this.clientFunctions[k] = eval("(function () { try { return " + data[k] + " } catch (ex) {console.log('wormholeExceptions:', ex, data[k]); return function(){};} }())");
 		this.clientFunctions[k].bindTo = (function (k) {
 			return function (func) {
 				self.clientFunctions[k].bound = func;
