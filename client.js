6a0fc5056d9ec8f7de67912edb4930e5f8dac1cc
Mathieu Gosbee <mail@matbee.com>
2013-08-05 21:46:08 -0400

    Client-side event emitter support

--- client.js
+++ client.js
@@ -1,4 +1,63 @@
+// EventEmitter support
+var EventEmitter = (typeof window !== 'undefined') ?
+  EventEmitter || {} :
+  exports;
+
+(function(exports) {
+  var slice = Array.prototype.slice;
+
+  var EventEmitter = function() {
+    this._listeners = {};
+  };
+
+  EventEmitter.prototype.on = function(name, fn) {
+    this._listeners[name] = this._listeners[name] || [];
+    this._listeners[name].push(fn);
+    this.emit("newListener", name, fn);
+    return this;
+  };
+
+  EventEmitter.prototype.remove = function(name, fn) {
+    fn && this._listeners[name] && this._listeners[name].splice(this._listeners[name].indexOf(fn), 1);
+    this.emit("removeListener", name, fn);
+  };
+
+  EventEmitter.prototype.emit = function(name) {
+
+    var listeners = this._listeners[name] || [],
+        args = slice.call(arguments, 1);
+    for(var i = 0, len = listeners.length; i < len; ++i) {
+      try {
+        listeners[i].apply(this, args);
+      } catch(err) {
+        this.emit('error', err);
+      }
+    }
+  };
+
+  EventEmitter.prototype.emits = function(name, fn) {
+    var ee = this;
+    return function() {
+      var args = slice.call(arguments),
+          result = fn.apply(this, args),
+          emit = result instanceof Array ? result : [result];
+
+      // destructuring emit
+      ee.emit.apply(ee, [name].concat(emit));
+      return result;
+    };
+  };
+
+  exports.EventEmitter = EventEmitter;
+  exports.global = new EventEmitter();
+  exports.emits = function() {
+    return exports.global.emits.apply(exports.global, slice.call(arguments));
+  };
+})(EventEmitter);
+
+
 var wormhole = function (socket) {
+	EventEmitter.EventEmitter.call(this);
 	this.clientFunctions = {};
 	this.serverFunctions = {};
 	this.socket = socket;
@@ -8,6 +67,7 @@ var wormhole = function (socket) {
 	var self = this;
 	this.setupSocket(socket);
 };
+wormhole.prototype.__proto__ = EventEmitter.EventEmitter.prototype;
 wormhole.prototype.charcodeArrayToString = function (arr) {
 	var string = "";
 	for (var i = 0; i < arr.length; i++) {
