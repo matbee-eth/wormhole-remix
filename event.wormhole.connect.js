06cf0ead96fc867376cd3af626abb88a3dfcd5b8
Mathieu Gosbee <mail@matbee.com>
2013-09-22 23:37:50 -0400

    .

---
+++ event.wormhole.connect.js
@@ -0,0 +1,38 @@
+var wormholeConnect = function () {
+	var module = {}, socket, io = module.exports = window.gnio = {};
+	THISSTRINGISTHESOCKETIOSCRIPTLOL;
+	this.io = io;
+};
+wormholeConnect.prototype.ready = function() {
+	var readyFunc = new Function(REPLACETHISSTRINGOKAY);
+	readyFunc();
+};
+wormholeConnect.prototype.connect = function(THISISTHECONNECTOBJECTOVERRIDE) {
+	THISISTHECONNECTOBJECTOVERRIDE = THISISTHECONNECTOBJECTOVERRIDE || {
+		reconnect: false,
+		'try multiple transports': true
+	};
+	if (this.io.sockets['THISSTRINGSHOULDCONTAINTHERIGHTHOSTNAMEOFTHISSERVER']) {
+		socket = this.io.sockets['THISSTRINGSHOULDCONTAINTHERIGHTHOSTNAMEOFTHISSERVER'];
+		socket.connect(THISISTHECONNECTOBJECTOVERRIDE);
+	} else {
+		socket = this.io.connect('THISSTRINGSHOULDCONTAINTHERIGHTHOSTNAMEOFTHISSERVER/THISISTHENAMESPACEFORSOCKETIO', THISISTHECONNECTOBJECTOVERRIDE);
+	}
+	
+	if (!window.wh) {
+		var wh = new wormhole(socket);
+		wh.ready(this.ready);
+		wh.setPath('THISISTHEHOSTNAMEOFTHESCRIPTSERVER');
+		window.wh = wh;
+	} else {
+		console.log("Wormhole exists!");
+		window.wh.setSocket(socket);
+		window.wh.setupSocket(socket);
+		window.wh.ready(this.ready);
+	}
+};
+
+(function () {
+	var connect = new wormholeConnect();
+	connect.connect(/*ThisIsTheConnectOverrideArgs*/);
+})();
