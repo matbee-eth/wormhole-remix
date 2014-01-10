eaf120820b4d67668e779d80ac3e5203f0275e68
Mathieuu Gosbee <mail@matbee.com>
2014-01-10 00:15:20 -0500

    test

--- event.js
+++ event.js
@@ -29,6 +29,9 @@ var wormhole = function (options) {
 		getChannelList: function (channel, cb) {
 			var traveller = this;
 			wormhole.getChannel(self._redisPubClient, channel, cb);
+		},
+		joinRTCChannel: function (channel, cb) {
+			this.joinRTCChannel(channel, cb);
 		}
 	};
 	this._clientMethods = {
